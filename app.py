9a47335dd0e7b59d38e381cfb0c66214f5f0f97d
lowannann <jajoanne.chi88@gmail.com>
2023-01-11 20:12:21 +0800

    added auto_number.html/modified css/footers

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from flask import request as flask_request
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
